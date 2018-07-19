aa9b3c64f15e50f7a0b1d6da78203888a7146da8
AlexScheller <alexgesc@gmail.com>
2018-07-18 22:21:24 -0700

    fully working, just need to comment better and clean up

--- static/javascript/cards.js
+++ static/javascript/cards.js
@@ -15,12 +15,17 @@ $(document).ready(function() {
 		},
 		add: function(card) {
 			this.cards.push(card);
+			console.log('adding');
+			console.log(this.cards);
 		},
 		draw: function(card) {
 			if (this.cards.length == 0) {
-				return "empty";
+				return 'empty';
 			}
-			return this.cards.pop(card);
+			let ret =  this.cards.pop(card);
+			console.log('drawing');
+			console.log(this.cards);
+			return ret
 		},
 		shuffle: function() {
 			let numCards = this.cards.length;
@@ -57,59 +62,52 @@ $(document).ready(function() {
 	deck.add('c3');
 	deck.add('c4');
 	deck.add('c5');
-	deck.shuffle();
+	deck.add('c6');
+	// deck.shuffle();
 
 	$.fn.flipCard = function() {
 		$(this).toggleClass('front-showing');
 		$(this).toggleClass('back-showing');
 	}
 
-	$.fn.finalizeDrawnCard = function() {
-		// NOTE that although for the purposes of
-		// this demonstration, cards are no longer
-		// droppables when finally drawn, it may make
-		// sense for them to remain droppable in order
-		// to become new piles or whatever.
-		$(this).droppable('disable');
+	$.fn.finalizeDraw = function() {
+		console.log('finalizing card draw');
+		$(this).flipCard();
 		$(this).removeClass('finalization-pending');
-		$(this).removeClass('back-showing');
-		$(this).addClass('front-showing');
 		$flipButton = $('<i class="fas fa-sync-alt flip-button"></i>');
+		$(this).prepend($flipButton);
 		$flipButton.on('click', function() {
 			$(this).parent().flipCard();
 		});
-		$(this).prepend($flipButton);
+		$(this).droppable('disable');
 	}
 
-	/* 
-	 * if there are cards left in the deck,
-	 * a card is assigned as a card spawner.
-	 * note that cardSpawner and cardSetup
-	 * are mutually recursive (on handlers).
-	 */
-	$.fn.cardSpawner = function() {
+	$.fn.handleCardDraw = function() {
+		console.log('handling card draw');
 		$(this).one('mousedown', function() {
+			$(this).removeClass('top-card');
 			$(this).addClass('finalization-pending');
-			$(this).removeClass('prototype');
-			$newCard = $(`
-				<div class="card back-showing draggable">
-					<div class="back">
-						<img src="static/assets/cardback.png">
-					</div>
-					<div class="front">
-						<img src="">
-					</div>
-				</div>
-			`);
-			$newCard.cardSetup();
+		});
+		$(this).one('mousedown', function() {
 			if (!deck.isEmpty()) {
-				$newCard.addClass('prototype');
+				$newCard = $(`
+					<div class="card back-showing draggable top-card">
+						<p class="data-holder" hidden=""></p>
+						<div class="back">
+							<img src="static/assets/cardback.png">
+						</div>
+						<div class="front">
+							<img src="">
+						</div>
+					</div>
+				`);
+				$newCard.setupTopCard();
+				$('#content-container').append($newCard)
 			}
-			$('#content-container').append($newCard)
 		});
 	}
 
-	$.fn.topOfDeckSetup = function() {
+	$.fn.handleDroppable = function() {
 		$(this).droppable({
 			hoverClass: "hovered-over",
 			over: function(event, ui) {
@@ -126,32 +124,44 @@ $(document).ready(function() {
 					$(ui.draggable).flipCard();
 				}
 			},
+			// NOTE: I attempted to use data attributes
+			// instead of a holding "p" div, but there
+			// a weird bug I couldn't figure out where
+			// removing a card, then dropping it, then
+			// dropping it again caused the top card to
+			// take on its orinal value, rather than the
+			// new top card. Haven't seen this bug with
+			// using a p div to hold the data.
 			drop: function(event, ui) {
 				console.log("dropped on me!");
+				let oldCardVal = $(ui.draggable).children('.data-holder').text();
+				deck.add($(this).children('.data-holder').text());
+				$(this).children('.data-holder').text(oldCardVal);
+				$(this).children('.front').children('img').attr(
+					'src', 'static/assets/' + oldCardVal + '.png'
+				);
+				$(ui.draggable).remove();
 			}
 		});
 	}
 
-	$.fn.cardSetup = function() {
-		$(this).topOfDeckSetup();
+	$.fn.setupTopCard = function() {
+		let newCardVal = deck.draw();
+		$(this).children('.data-holder').text(newCardVal);
 		$(this).children('.front').children('img').attr(
-			'src', 'static/assets/' + deck.draw() + '.png'
+			'src', 'static/assets/' + newCardVal + '.png'
 		);
-		if (!deck.isEmpty()) {
-			$(this).cardSpawner();
-		}
+		$(this).handleCardDraw();
 		$(this).one('mouseup', function() {
-			$(this).finalizeDrawnCard();
-		});
-		$(this).children('.flip-button').on('click', function() {
-			$(this).parent().flipCard();
+			$(this).finalizeDraw();
 		});
 		$(this).draggable({
 			containment: 'parent',
 			stack: '.draggable'
 		});
+		$(this).handleDroppable();
 	}
 
-	$('.prototype').cardSetup();
+	$('.top-card').setupTopCard();
 
 });
